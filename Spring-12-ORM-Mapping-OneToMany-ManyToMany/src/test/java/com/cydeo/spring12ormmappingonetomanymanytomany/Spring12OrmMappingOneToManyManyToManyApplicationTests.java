9de7ba0b79e7fc1a9bf32b20ed6704e410021dbe
Zamora <jzamora.jd@gmail.com>
2024-06-22 23:12:49 -0500

    added Spring 12 module for one to many to many sql class

---
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/test/java/com/cydeo/spring12ormmappingonetomanymanytomany/Spring12OrmMappingOneToManyManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring12ormmappingonetomanymanytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12OrmMappingOneToManyManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
