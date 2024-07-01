9711c832082166514d46effbb33cf6295d29f56b
Zamora <jzamora.jd@gmail.com>
2024-07-01 17:16:02 -0500

    added customer table

--- Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Customer.java
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Customer.java
@@ -1,4 +1,27 @@
 package com.cydeo.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String name;
+    private String surName;
+    private String email;
+    private String address;
+
+    @OneToMany
+    private List<Payment> paymentList;
 }
