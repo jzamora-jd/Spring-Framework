ddbd03bfaf38a2774f9aa0dcafa79d01eff84ba6
Zamora <jzamora.jd@gmail.com>
2024-07-07 16:42:38 -0500

    mapping

--- Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Cart.java
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Cart.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "carts")
@@ -14,4 +15,13 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToMany
+    @JoinTable(name = "cart_item_rel",
+            joinColumns = @JoinColumn(name="c_id"),
+            inverseJoinColumns = @JoinColumn(name = "i_id")
+    )
+
+    private List<Item> itemList;
+
 }
--- Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Item.java
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Item.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "items")
@@ -13,4 +14,15 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToMany(mappedBy = "itemList")
+    private List<Cart> cart;
+
+    public Item(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
