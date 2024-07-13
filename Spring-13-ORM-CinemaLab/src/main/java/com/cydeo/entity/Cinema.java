e5eaef717ff71735ae326d2bcf8ba2bcd32126dd
Zamora <jzamora.jd@gmail.com>
2024-07-13 16:12:50 -0500

    added Account, Ticket and User table

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -1,6 +1,8 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +15,7 @@ public class Cinema extends BaseEntity{
     private String name;
     private String sponsoredName;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     private Location location;
 
 
