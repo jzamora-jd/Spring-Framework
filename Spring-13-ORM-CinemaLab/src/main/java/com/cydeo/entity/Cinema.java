8441a60ace2050d687142cb771d14b5cc06c8b1e
Zamora <jzamora.jd@gmail.com>
2024-07-13 16:01:23 -0500

    added CinemaTables

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Cinema.java
@@ -0,0 +1,19 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cinema extends BaseEntity{
+    private String name;
+    private String sponsoredName;
+
+    private Location location;
+
+
+}
